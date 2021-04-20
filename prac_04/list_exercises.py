f780b42da7ec9f817836abd33f2cbf575f24ba78
Khun-blip <81811807+Khun-blip@users.noreply.github.com>
2021-04-20 17:16:10 +0800

    listy

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,11 @@
+num1=int(input("Number:"))
+num2=int(input("Number:"))
+num3=int(input("Number:"))
+num4=int(input("Number:"))
+num5=int(input("Number:"))
+avg=(num1+num2+num3+num4+num5)/5
+if num1:
+    print("The first number is ", num1)
+if num5:
+    print("The last number is", num5)
+    print("The average of the number is", avg)
---
+++ prac_04/total_income.py
@@ -0,0 +1,17 @@
+def main():
+    """Display earnings report for incomes over a given number of months."""
+    incomes = []
+    months = int(input("How many months? "))
+
+    for month in range(1, months + 1):
+        earnings = float(input("Enter earnings for month " + str(month) + ": "))
+        incomes.append(earnings)
+
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, months + 1):
+        earnings = incomes[month - 1]
+        total += earnings
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, earnings, total))
+
+main()
